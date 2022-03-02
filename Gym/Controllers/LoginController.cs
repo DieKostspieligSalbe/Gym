c7928ba19cc0f355dfbe3536425bbbe2e021bdbb
admin <admin@DESKTOP-9FGO3K9>
2022-03-03 02:12:45 +0300

    Add project files.

---
+++ Gym/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using Gym.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace Gym.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index() //page where you login
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProcessLogin([FromForm] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                User foundUser = UserRepository.userList.FirstOrDefault(u => u.Login == user.Login && u.Password == user.Password);
+                if (foundUser != null)
+                {
+                    await Authenticate(user.Login); 
+
+                    return View("AuthSuccess", user);  //will lead to personal training plans
+                }
+                ModelState.AddModelError("", "Incorrect login or password");
+            }
+            return View("FailedAuth");          
+        }
+
+        private async Task Authenticate(string userLogin)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userLogin)
+            };
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
+    }
+}
---
+++ Gym/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gym.Models
+{
+    public class User
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string Login { get; set; }
+        [Required,DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+
+    public static class UserRepository
+    {
+        public static List<User> userList { get; set; } = new();
+
+        static UserRepository()
+        {
+            userList.Add(new User() { Login = "Admin", Password = "Password" });
+            userList.Add(new User() { Login = "User", Password = "Pass" });
+        }
+
+    }
+}
