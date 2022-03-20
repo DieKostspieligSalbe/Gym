91bc0d442d68ed1a8f7c571ed44c4fcba54d995f
Alexandra Mikhailova <morgen270@live.com>
2022-03-21 02:27:48 +0300

    onhover and onclick on home/index

--- Gym/wwwroot/js/site.js
+++ Gym/wwwroot/js/site.js
@@ -5,13 +5,35 @@
 const bodyWrapper = document.getElementById("body_wrapper")
 
 
-const handleMuscleHover = (e) => {
-
+const handleMuscleActivation = (e) => {
     const hovMuscle = e.target.parentNode
     const hovMuscleInstances = document.querySelectorAll(`[data-muscle=${hovMuscle.dataset.muscle}] path`)
-    Array.from(hovMuscleInstances)
-        .forEach(muscle => muscle.classList.toggle('muscle--hover'))
+    console.log(hovMuscleInstances)
+    Array.from(hovMuscleInstances).forEach(muscle => muscle.classList.toggle(e.type === 'mousedown' ? 'muscle--open' : 'muscle--hover'))
 }
 
-bodyWrapper.addEventListener('mouseover', handleMuscleHover)
-bodyWrapper.addEventListener('mouseout', handleMuscleHover)
+bodyWrapper.addEventListener('mouseover', handleMuscleActivation)
+bodyWrapper.addEventListener('mouseout', handleMuscleActivation)
+bodyWrapper.addEventListener('mousedown', handleMuscleActivation)
+
+//const handleMuscleHover = (e) => {
+
+//    const hovMuscle = e.target.parentNode
+//    const hovMuscleInstances = document.querySelectorAll(`[data-muscle=${hovMuscle.dataset.muscle}] path`)
+//    Array.from(hovMuscleInstances)
+//        .forEach(muscle => muscle.classList.toggle('muscle--hover'))
+//}
+
+//const handleMuscleClick = (e) => {
+//    const hovMuscle = e.target.parentNode
+//    console.log("clicked node is", hovMuscle)
+//    const hovMuscleInstances = document.querySelectorAll(`[data-muscle=${hovMuscle.dataset.muscle}] path`)
+//    console.log(hovMuscleInstances)
+//    Array.from(hovMuscleInstances).forEach(muscle => muscle.classList.toggle('muscle--open'))
+//}
+
+//bodyWrapper.addEventListener('mouseover', handleMuscleHover)
+//bodyWrapper.addEventListener('mouseout', handleMuscleHover)
+
+//bodyWrapper.addEventListener('mousedown', handleMuscleClick)
+
